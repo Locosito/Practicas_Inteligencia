7703de6cd892a0878595cd895b53fc63ff88ea07
Locosito <joshua_luquevilela@hotmail.com>
2019-09-09 21:23:55 -0500

    Practica 1

---
+++ Practica1/Assets/scripts/agent.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class agent : MonoBehaviour
+{
+    Vector3 velocity = Vector3.zero;
+    Vector3 desired = Vector3.zero;
+    Vector3 steer = Vector3.zero;
+    float maxSpeed = 5;
+    float maxSteer = 15;
+
+    Transform target;
+
+    void Update()
+    {
+        target = GameObject.Find("Target").transform;
+
+        // desired
+        desired = -(target.position - transform.position).normalized * maxSpeed;
+        //steer
+        steer = Vector3.ClampMagnitude(desired - velocity, maxSteer);
+        //velocity
+        velocity += steer * Time.deltaTime;
+        transform.position += velocity * Time.deltaTime;
+    }
+}
+
+/*
+    float tx = 0;
+    float ty = 0;
+
+    Update
+
+    tx += 0.01f;
+    ty += 0.01f;
+
+    float x = -5 + 10 * Mathf.PerlinNoise(tx,0);
+    float y = -5 + 10 * Mathf.PerlinNoise(0,ty);
+    transform.position = new Vector(x,y,0);
+
+
+ */
---
+++ Practica1/Assets/scripts/circulo.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class circulo : MonoBehaviour
+{
+
+    float radius1 = 1;
+    float radius2 = 15;
+
+    GameObject agent2;
+    Transform agent;
+
+    void Start()
+    {
+        agent = GameObject.Find("agent").transform;
+        agent2 = GameObject.Find("agent").gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        Vector3 distancia = agent.position - transform.position;
+        float sqrLen = distancia.sqrMagnitude;
+
+        if(sqrLen <= 2){
+            Destroy(agent2);
+            print("ganaste");
+        }
+        else if(sqrLen > 200){
+            Destroy(agent2);
+            print("perdiste");
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, radius1);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, radius2);
+    }
+}
